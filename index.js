08194e5e03079c1b5c7b824c4c3eda1a1b678ee3
karan <meghawalkaran@gmail.com>
2022-10-18 17:53:40 +0530

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let sidemenu = document.getElementById("sidemenu");
 let menubtn = document.getElementById("menu");
 function openmenu(){
-    sidemenu.style.right ="130px"
+    sidemenu.style.top ="0px"
     menubtn.style.display = "none"
 
 }
 function closemenu(){
-    sidemenu.style.right ="-130px"
+    sidemenu.style.top ="-915px"
     menubtn.style.display = "block"
 }
+
+let About = document.getElementById("about")
