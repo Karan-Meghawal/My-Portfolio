4532e6a4338dc21d4dcae5b5417bfb0ee3aa026e
Karan-Meghawal <meghawalkaran@gmail.com>
2022-11-15 12:18:49 +0530

    Update for responsive and animation name

--- index.js
+++ index.js
@@ -3,14 +3,22 @@
 
 let sidemenu = document.getElementById("sidemenu");
 let menubtn = document.getElementById("menu");
+let closebtn = document.getElementById("xmark")
 function openmenu(){
-    sidemenu.style.top ="0px"
+    sidemenu.style.top ="-5px"
     menubtn.style.display = "none"
+    closebtn.style.display = "block"
 
 }
 function closemenu(){
-    sidemenu.style.top ="-915px"
+    sidemenu.style.top ="-980px"
     menubtn.style.display = "block"
+    closebtn.style.display = "none"
 }
 
-let About = document.getElementById("about")
+var typed = new Typed(".auto-type",{
+    strings:["Full Stack Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
